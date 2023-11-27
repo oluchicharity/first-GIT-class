c9449cd6a158681352f9cb72384500c45ec3184a
Agbakwuru Oluchi <lulu@Agbakwurus-MacBook-Air.local>
2023-11-27 17:35:33 +0100

    set up server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("./config/config")
+require ("dotenv").config();
+const studentRouter= require("./routers/studentrouters")
+const app = express();
+app.use(express.json());
+app.use("/api/student", studentRouter)
+const port= process.env.port
+
+app.listen(port,()=>{
+console.log(`listening on port: ${port}: alive and active`)
+})
